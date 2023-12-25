4af05086b5b68acd9ec514a8e1a4aee6056ceccb
Hyunji Kim <s2208@e-mirim.hs.kr>
2023-12-25 16:44:44 +0900

    로그인 화면 구현 중

---
+++ app/src/main/java/kr/hs/emirim/dreamorreality/MainActivity.kt
@@ -0,0 +1,20 @@
+package kr.hs.emirim.dreamorreality
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.hs.emirim.dreamorreality.ui.theme.DreamOrRealityTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signin)
+    }
+}
