a6097c66a6beeab600025a3eafd5da7c5380482e
Hyunji Kim <s2208@e-mirim.hs.kr>
2023-12-27 16:40:10 +0900

    뭘한건지도 모르겠다...

--- app/src/main/java/kr/hs/emirim/dreamorreality/SignUpActivity.kt
+++ app/src/main/java/kr/hs/emirim/dreamorreality/SignUpActivity.kt
@@ -15,74 +15,26 @@ class SignUpActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
-        // 각 스피너 초기화
-        val genderSpinner: Spinner = findViewById(R.id.genderSpinner)
-        val gradeSpinner: Spinner = findViewById(R.id.gradeSpinner)
-        val classSpinner: Spinner = findViewById(R.id.classSpinner)
-        val numberSpinner: Spinner = findViewById(R.id.numberSpinner)
-        val emailSpinner: Spinner = findViewById(R.id.emailSpinner)
-        val yearSpinner : Spinner = findViewById(R.id.yearSpinner)
-
-        // 어댑터 생성
-        val gradeAdapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.grade_array,
-            android.R.layout.simple_spinner_item
-        )
-        val classAdapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.class_array,
-            android.R.layout.simple_spinner_item
-        )
-        val numberAdapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.number_array,
-            android.R.layout.simple_spinner_item
-        )
-        val emailAdapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.email_array,
-            android.R.layout.simple_spinner_item
-        )
-        val yearAdapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.year_array,
-            android.R.layout.simple_spinner_item
-        )
-
-        // 드롭다운 레이아웃 설정
-        gradeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        classAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        numberAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        emailAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        // 스피너에 어댑터 설정
-        gradeSpinner.adapter = gradeAdapter
-        classSpinner.adapter = classAdapter
-        numberSpinner.adapter = numberAdapter
-        emailSpinner.adapter = emailAdapter
-        yearSpinner.adapter = yearAdapter
+        //필요 필드 값을 가져옴.
+        val username = findViewById<EditText>(R.id.signUpEditTextName).text.toString()
+        val userid = findViewById<EditText>(R.id.signUpEditTextID).text.toString()
+        val userpw = findViewById<EditText>(R.id.signUpEditTextPW).text.toString()
+        val checkpw = findViewById<EditText>(R.id.checkEditTextPW).text.toString()
+        val usergrade = getSelectedSpinnerItem(R.id.gradeSpinner) + getSelectedSpinnerItem(R.id.classSpinner) + getSelectedSpinnerItem(R.id.numberSpinner)
+        val useremail = getEmail()
+        val usergraduation = getSelectedSpinnerItem(R.id.yearSpinner)
+        val userdream = findViewById<EditText>(R.id.signupEditTextDream).text.toString()
 
         val nextButton = findViewById<Button>(R.id.go_nextBtn)
         nextButton.setOnClickListener {
-            //필요 필드 값을 가져옴.
-            val nameEditText = findViewById<EditText>(R.id.signUpEditTextName)
-            val idEditText = findViewById<EditText>(R.id.signUpEditTextID)
-            val pwEditText = findViewById<EditText>(R.id.signUpEditTextPW)
-            val checkPwEditText = findViewById<EditText>(R.id.checkEditTextPW)
-
-            val signupNAME = nameEditText.text.toString()
-            val signupID = idEditText.text.toString()
-            val signupPW = pwEditText.text.toString()
-            val signupCheckPW = checkPwEditText.text.toString()
 
             //필드가 하나라도 비었다면 Error Dialog 발생
-            if(signupNAME.isEmpty() || signupID.isEmpty() || signupPW.isEmpty() || signupCheckPW.isEmpty())
+            if(username.isEmpty() || userid.isEmpty() || userpw.isEmpty() || checkpw.isEmpty()
+                || usergrade.isEmpty() || useremail.isEmpty() || usergraduation.isEmpty() || userdream.isEmpty())
                 showErrorDialog("모든 필드를 입력해주세요.")
 
             //비밀번호가 다시 입력한 값과 일치하지 않다면 Error Dialog 발생
-            if(signupPW != signupCheckPW) showErrorDialog("비밀번호가 일치하지 않습니다.")
+            if(userpw != checkpw) showErrorDialog("비밀번호가 일치하지 않습니다.")
         }
     }
     private fun showErrorDialog(message: String) {
@@ -93,4 +45,21 @@ class SignUpActivity : ComponentActivity() {
         val dialog = builder.create()
         dialog.show()
     }
+
+    //학번 조합 메소드
+    private fun getSelectedSpinnerItem(spinnerId: Int): String {
+        val spinner = findViewById<Spinner>(spinnerId)
+        return spinner.selectedItem.toString()
+    }
+
+    //이메일 조합 메소드
+    private fun getEmail(): String {
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val emailSpinner = findViewById<Spinner>(R.id.emailSpinner)
+        val emailProvider = emailSpinner.selectedItem.toString()
+
+        return emailEditText.text.toString() + "@" + emailProvider
+
+    }
+
 }
