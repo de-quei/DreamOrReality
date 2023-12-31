6fbf91b54185c099e402a93441240b2a0c320e56
Hyunji Kim <s2208@e-mirim.hs.kr>
2023-12-31 15:41:30 +0900

    rollback

--- app/src/main/java/kr/hs/emirim/dreamorreality/HomeActivity.kt
+++ app/src/main/java/kr/hs/emirim/dreamorreality/HomeActivity.kt
@@ -14,6 +14,12 @@ class HomeActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val welcomeTextView = findViewById<TextView>(R.id.welcomeTextView)
+
+        // Intent에서 사용자 이름을 가져와서 TextView에 설정
+        val username = intent.getStringExtra("username")
+        welcomeTextView.text = "$username 님"
+
         val home = findViewById<ImageButton>(R.id.home_btn)
         val study = findViewById<ImageButton>(R.id.study_btn)
         val chat = findViewById<ImageButton>(R.id.chat_btn)
